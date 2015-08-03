6d3c7fac658e8fcb27bf3f82b5fe09fba2df9d26
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-03 19:00:36 +0200

    Create product in migration through product extension class name.

--- dummy/db/migrate/20140404165919_add_proposition_product.voluntary_proposition_engine.rb
+++ dummy/db/migrate/20140404165919_add_proposition_product.voluntary_proposition_engine.rb
@@ -1,13 +1,13 @@
 # This migration comes from voluntary_proposition_engine (originally 20140404164145)
 class AddPropositionProduct < ActiveRecord::Migration
   def up
-    if product = Product.where(name: 'Proposition').first
+    if product = Product::Proposition.first
     else
-      Product.create(name: 'Proposition', text: 'Product about proposition development process with delegated voting system (Liquid Democracy)') 
+      Product::Proposition.create(name: 'Proposition', text: 'Product about proposition development process with delegated voting system (Liquid Democracy)') 
     end
   end
   
   def down
-    product.destroy if product = Product.where(name: 'Proposition').first
+    product.destroy if product = Product::Proposition.first
   end
 end
